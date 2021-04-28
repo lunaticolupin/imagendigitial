2ed67c77ad431eb24bb3d66c538a72ada067247b
Alejandro <lunaticolupin@outlook.com>
2021-04-27 19:19:50 -0500

    Modificación Diferencia

--- 05/resta.py
+++ 05/resta.py
@@ -14,30 +14,21 @@ temp = np.copy(imagen1)
 largo = len(temp)
 ancho = len(temp[0])
 
-for i in range(1, largo):
+for i in range(0, largo):
     color = [0,0,0]
-    for j in range(1, ancho): 
+    for j in range(0, ancho): 
         color[0] = imagen1[i,j,0]*1 - imagen2[i,j,0]*1
         color[1] = imagen1[i,j,1]*1 - imagen2[i,j,1]*1
         color[2] = imagen1[i,j,2]*1 - imagen2[i,j,2]*1
-
-        if (color[0]<0):
-            color[0]=0
-
-        if (color[1]<0):
-            color[1]=0
-
-        if (color[2]<0):
-            color[2]=0
             
-        temp[i,j,0] = color[0]
-        temp[i,j,1] = color[1]
-        temp[i,j,2] = color[2]
+        temp[i,j,0] = abs(color[0])
+        temp[i,j,1] = abs(color[1])
+        temp[i,j,2] = abs(color[2])
 
 cv.imshow('Suma de Imagenes',temp)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
-nombre = "operador_xor_2.png"
+nombre = "segmentacion.png"
 cv.imwrite(nombre, temp)
 
